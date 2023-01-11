3a0cb059ccc45e655c2306b58d3f31a2d38c4957
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-01-11 19:42:42 -0300

    arreglos varios

--- busqueda_binaria.py
+++ busqueda_binaria.py
@@ -1,7 +1,7 @@
 import random
 
 def busqueda_binaria(lista, comienzo, final, objetivo):
-
+    print(f'buscando {objetivo} entre {lista[comienzo]} y {lista [final-1]}')
     if comienzo > final:
         return False
     
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     tamano_de_lista = int(input('de que tamano es la lista?'))
     objetivo = int(input ('que numero quieres encontrar?'))
 
-    lista = sorted([random.randint(0,100) for i in range (tamano_de_lista)])
+    lista = sorted([random.randint(0,10000) for i in range (tamano_de_lista)])
     
     encontrado = busqueda_binaria(lista, 0, len(lista), objetivo)
 
